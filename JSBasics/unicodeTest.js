edcaa98364ae80ac097c85d655576983cdebc055
Lucas <lucas.pineda@hotmail.com>
2019-01-29 23:55:07 -0200

    Create unicodeTest.js

---
+++ JSBasics/unicodeTest.js
@@ -0,0 +1,11 @@
+
+//em unicode, os valores seguem a seguinte ordem: Números -> Letras Maius. -> Letras Min.
+var unicodeTest1 = 'A' > '1'
+
+var unicodeTest2 = 'a' > 'A'
+
+var unicodeTest3 = 'a' > '1'
+
+console.log(unicodeTest1);
+console.log(unicodeTest2);
+console.log(unicodeTest3);
