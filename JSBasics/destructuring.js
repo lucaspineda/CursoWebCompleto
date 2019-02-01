5cef309128e17bbe28ab9b6ff9759b387ea32a22
Lucas <lucas.pineda@hotmail.com>
2019-02-01 00:20:54 -0200

    Update destructuring.js

--- JSBasics/destructuring.js
+++ JSBasics/destructuring.js
@@ -14,3 +14,9 @@ const {endereco: {rua,numero,cidade}} = pessoa;
 console.log(rua, numero, cidade);
 //console.log(rua, numero, cidade, estado); resulta em erro pois não estava dentro do destructuring
  
+// Este também é um operador de destructuring, porém para arrays
+const[destrucArray] = [10];
+console.log(destrucArray);
+
+const[a, b, ,d , e = 0] = [1,2,3,4];
+console.log(a,b,d,e);
