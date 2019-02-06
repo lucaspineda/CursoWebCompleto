3d13042a00b12bc19c7181aae576237c954c55b5
Lucas <lucas.pineda@hotmail.com>
2019-02-06 21:24:20 -0200

    Create parametroPadrao.js

---
+++ Funções/parametroPadrao.js
@@ -0,0 +1,8 @@
+
+
+function padrao (a = 1, b = 2, c = 3){
+    return a + b + c;
+}
+
+console.log(padrao()); //faz soma dos valores padroes
+console.log(padrao(3)); //imprime 8, pois retorna 3 + 2 + 3
