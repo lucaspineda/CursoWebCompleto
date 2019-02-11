eb2e6fa9e2c20ca09fa92d994ae43fcce2d84966
Lucas <lucas.pineda@hotmail.com>
2019-02-11 08:02:35 -0200

    Update funcAnonima.js

--- Funções/funcAnonima.js
+++ Funções/funcAnonima.js
@@ -4,3 +4,13 @@ const anonima = function(){
 }
 
 anonima();
+
+
+
+const imprimirResultado = function(a, b){
+    //console.log(a + b)
+}
+
+imprimirResultado(1,2, function(x,y){
+    return x-y
+})
