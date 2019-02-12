b4fb1c31cf999ea166051f09f4a8c96ecffc3414
Lucas <lucas.pineda@hotmail.com>
2019-02-12 19:50:55 -0200

    Update funcAnonima.js

--- Funções/funcAnonima.js
+++ Funções/funcAnonima.js
@@ -5,12 +5,14 @@ const anonima = function(){
 
 anonima();
 
+const soma = function(a,b) {
+    return a + b;
+} 
 
-
-const imprimirResultado = function(a, b){
-    //console.log(a + b)
+const imprimirResultado = function(a, b, operacao = soma){
+    console.log(operacao(a,b))
 }
 
-imprimirResultado(1,2, function(x,y){
-    return x-y
-})
+imprimirResultado(1,2, function(x,y){ //invocando uma função anonima como parametro
+    return x - y;
+}) //imprime - 1 pois a função anonima se refere ao parametro "operacao" do metodo acima
