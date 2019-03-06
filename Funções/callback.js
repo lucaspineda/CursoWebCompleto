8c4e4156917228c023249d447e009d38d98420c5
Lucas <lucas.pineda@hotmail.com>
2019-03-06 08:00:12 -0300

    Update callback.js

--- Funções/callback.js
+++ Funções/callback.js
@@ -1,6 +1,6 @@
 // Callback é uma função que é usada como "callback". Ela é tipicamente passada como argumento de outra função e/ou chamada quando um evento for acontecido, ou quando uma parte de código receber uma resposta de que estava à espera.
 
-// Isto é muito comum em javascript lado cliente e servidor (NodeJS) mas não só. A função callback é muitas vezes assíncrona na medida em que é chamada como consequência de outro código que esta a correr no background. A grande vantagem é que assim o computador pode ir processando outros processos enquanto a resposta não chega e não precisa assim de parar tudo à espera dessa resposta.
+// Isto é muito comum em javascript lado cliente e servidor (NodeJS) mas não só. A função callback é muitas vezes assíncrona na medida em que é chamada como consequência de outro código que está a correr no background. A grande vantagem é que assim o computador pode ir processando outros processos enquanto a resposta não chega e não precisa assim de parar tudo à espera dessa resposta.
 
 // Um exemplo clássico é um auscultador de eventos que chama uma função quando o evento acontece:
 
