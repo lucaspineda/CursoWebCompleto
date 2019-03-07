43e8a34f0ef53d004265359971fca518d39c66b1
Lucas <lucas.pineda@hotmail.com>
2019-03-07 20:33:41 -0300

    Update classeVsFactory.js

--- Funções/classeVsFactory.js
+++ Funções/classeVsFactory.js
@@ -10,3 +10,12 @@ class Pessoa {
 
 const  p1 = new Pessoa('João')
 p1.falar();
+
+
+const criarPessoa = (nome) => {
+    return {
+    falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+criarPessoa('Joãoo').falar()
