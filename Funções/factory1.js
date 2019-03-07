54d27adf390d667fc82d446f66cec2158ac1247a
Lucas <lucas.pineda@hotmail.com>
2019-03-06 21:50:27 -0300

    Create factory1.js

---
+++ Funções/factory1.js
@@ -0,0 +1,10 @@
+// função factory é uma função que retorna um objeto
+
+function criarPessoa(){
+    return {
+        nome: 'lucas',
+        sobrenome: 'pineda'
+    }
+}
+
+console.log(criarPessoa())
