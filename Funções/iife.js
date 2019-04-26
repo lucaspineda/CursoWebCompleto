22d3302b98d98379652b745071b30b9040de45f5
Lucas <lucas.pineda@hotmail.com>
2019-04-25 23:32:39 -0300

    update files

--- Funções/iife.js
+++ Funções/iife.js
@@ -1,11 +1,13 @@
 // IIFE = Immediately invoked function expression
 
 let x = 20;
+console.log(x);
 
 (function(){
-    x = 3;
+    var x = 3;
     console.log('Será executado na hora')
     console.log('foge do escopo global')
     console.log(x)
     
 })()
+console.log(x) // ignore escopo dentro da funcao e pega escopo de fora
---
+++ array/test.js
@@ -0,0 +1,9 @@
+const a = [5,9];
+const b = [9,8];
+
+console.log (a + b)
+
+
+var apt = 2;
+apt = apt<<2;
+console.log(apt)
