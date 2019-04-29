1718fde13361be649eb160d73a20cda91d32cb36
Lucas <lucas.pineda@hotmail.com>
2019-04-28 23:13:11 -0300

    atualização do curso

--- array/simulandoReduce.js
+++ array/simulandoReduce.js
@@ -1,10 +1,11 @@
-
-Array.prototype.reduce2 = function(callback){
-    let acumulador = this [0];
-    for(index = 0; index = this.length; index++){
-        acumulador = (callback(this[index], this[index+1]))
+    
+Array.prototype.reduce2 = function(callback, valorInicial) {
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for (let i = indiceInicial; i < this.length; i++) {
+        acumulador = callback(acumulador, this[i], i, this)
     }
-    return resultado;
+    return acumulador
 }
 
 
