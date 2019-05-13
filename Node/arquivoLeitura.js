2a99c8df88328a8696d0611e7a20a43618ea90ba
Lucas <lucas.pineda@hotmail.com>
2019-05-12 22:22:05 -0300

    Aula sobre leitura de arquivo

--- Node/arquivoLeitura.js
+++ Node/arquivoLeitura.js
@@ -1,9 +1,25 @@
-const FileSystem = require('fs') //módulo que já vem previamente instalado no node para leitura de arquivos
+const fileSystem = require('fs') //módulo que já vem previamente instalado no node para leitura de arquivos
 
 const caminho = __dirname + '/arquivo.json'
 
 //ler de forma sincrona:
-const conteudo = FileSystem.readFileSync(caminho, 'utf-8')
+const conteudo = fileSystem.readFileSync(caminho, 'utf-8')
 console.log(conteudo)
 
 //Assíncrona
+fileSystem.readFile(caminho, 'utf-8', (err, conteudo) =>{
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+})
+
+//Forma mais simples de fazer, já que o arquivo é um JSON
+
+const config = require('./arquivo.json')
+console.log(config.db)
+
+//ler todos arquivos da pasta
+fileSystem.readdir(__dirname, (err, arquivos) =>{
+    console.log("Consteúdo da pasta:")
+    console.log(arquivos)
+})
+
