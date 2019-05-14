f2dc378a567bad60b7158bdb31b0ae526cd35d0b
Lucas <lucas.pineda@hotmail.com>
2019-05-13 23:51:20 -0300

    aula sobra padrao middleware

--- Node/middlewares.js
+++ Node/middlewares.js
@@ -4,3 +4,10 @@ const passo1 = (ctx, next) =>{
     ctx.valor1 = 'mid1'
     next()
 }
+const passo2 = (ctx, next) =>{
+    ctx.valor2 = 'mid2'
+    next()
+}
+const passo3 = ctx =>{
+    ctx.valor2 = 'mid3'
+}
